f6e44af5fc3369674cbbbd53ea8a4e150db8d4b0
puge <mail@puge.org>
2020-10-19 08:57:37 +0800

    save

--- aliveClient.py
+++ aliveClient.py
@@ -1,21 +1,28 @@
 import requests
 import json
 import time
-from subprocess import Popen, PIPE
+import psutil
 
 url = "http://t.lamp.run/alive"
 
 def getIPAddrs():
-  p = Popen("hostname -I", shell=True, stdout=PIPE)
-  data = p.stdout.read() # 获取命令输出内容
-  data = str(data,encoding = 'UTF-8') # 将输出内容编码成字符串
-  ip_list = data.split(' ') # 用空格分隔输出内容得到包含所有IP的列表
-  if "\n" in ip_list: # 发现有的系统版本输出结果最后会带一个换行符
-      ip_list.remove("\n")
-  print(ip_list)
-  return ip_list
+  """
+  通过给定前缀跨平台查询ip
+  :param prefix:  需要查询的ip前缀
+  :return: ip地址
+  """
+  localIP = []
+  dic = psutil.net_if_addrs()
+  for adapter in dic:
+    snicList = dic[adapter]
+    for snic in snicList:
+      if not snic.family.name.startswith('AF_INET'):
+        continue
+      localIP.append(snic.address)
 
-ip_list = getIPAddrs()
+  return localIP
+
+ip_list = ','.join(getIPAddrs())
 
 class AliveClient(object):
   def __init__ (self, name = None, interval = None):
@@ -25,11 +32,10 @@ class AliveClient(object):
     payload = json.dumps({
       "name": self.name,
       "endTime": int(round(time.time() * 1000)) + self.interval,
-      "message": '机器IP:' + str(ip_list)
+      "message": '',
+      "IP": ip_list
     })
-    headers = {
-      'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
 
     response = requests.request("POST", url, headers=headers, data = payload)
 
--- index.py
+++ index.py
@@ -22,7 +22,7 @@ def login():
   print(response.text)
 
 # 先自动登录网络
-login()
+# login()
 
 weixin = Weixin(corpid="ww7192afc91d2f618a", corpsecret="lmLChwwkbfleWMUxJKkk2QLp121Lse-BU92Z08fC_fc")
 alive = AliveClient(name="全媒体抢单监控", interval = 5 * 60 * 1000)
@@ -32,7 +32,7 @@ cookiesData = ''
 
 def getcookie():
   global cookiesData
-  response = requests.post("https://project.peopleurl.cn/php/interface/login.php", {'username': '蒲鸽', 'password': 'MMit7750'})
+  response = requests.post("https://project.peopleurl.cn/interface/public/index.php/user/login", json.dumps({"username": "蒲鸽", "password": "MMit7750"}))
   print(response.text)
   #获取response中的cookies
 
@@ -53,55 +53,19 @@ data.close()
 # 获取cookie
 getcookie()
 
-# 数据清理
-def clear(text):
-  text = text.replace('\r', '')
-  text = text.replace('\n', '')
-  text = text.replace('\t', '')
-  return text
+def ddxx(item):
+  return "申请部门: %s\n联系方式: %s\n完成时间: %s\n详细需求: %s\n需求描述: %s\n" % (item['dep_name'], item['contact'], item['finish_time_f'], item['form_type_name'], item['description'])
 
-
-def ddxx(id):
-  cookie_jar = RequestsCookieJar()
-  cookie_jar.set("PHPSESSID", cookiesData["PHPSESSID"], domain="project.peopleurl.cn")
-  cookie_jar.set("sso_s", cookiesData["sso_s"], domain="peopleurl.cn")
-  cookie_jar.set("sso_u", cookiesData["sso_u"], domain="peopleurl.cn")
-  response = requests.get("https://project.peopleurl.cn/partyb/bid.php?id=" + id, cookies = cookie_jar)
-  # print(response.text)
-
-  # 分析处理数据
-  # 申请人
-  sqr = Tool.subString(response.text, '<td>申请人</td><td><input type="text" value="', '" id=')
-  sqbm = Tool.subString(response.text, '<td>申请部门</td><td><input type="text" value="', '" id=')
-  lxfs = Tool.subString(response.text, '<td>联系方式</td><td><input type="text" id="contact" value="', '" readonly')
-  wcsj = Tool.subString(response.text, '<td>完成时间</td><td><input type="text" id="dealtime" value="', '" readonly')
-  xxxq = Tool.subString(response.text, '<p><span>详细需求：</span></p>', '</div>')
-  xqms = Tool.subString(response.text, '<textarea id="description" readonly>', '</textarea>')
-  return "申请人: %s\n申请部门: %s\n联系方式: %s\n完成时间: %s\n详细需求: %s\n需求描述: %s\n" % (sqr, sqbm, lxfs, wcsj, xxxq, xqms)
-
-def weiqiang(text):
-  dataList = Tool.subStringArr(text, '<tr>', '</tr>')
+def weiqiang(dataList):
   returnData = ''
   for item in dataList:
-    itemList = Tool.subStringArr('sdsd' + item, '<td', '</td>')
-    itemID = itemList[0]
-    itemID = itemID.replace('>', '')
-    if not itemID.isdigit():
-      print("跳过异常订单: " + itemID)
-      continue
+    itemID = item['id']
     if (itemID not in orderList):
       orderList[itemID] = True
-      name = itemList[1]
-      name = name.replace('<a href="order.php?id=' + itemID + '">', '')
-      name = name.replace('</a>', '')
-      name = name.replace('>', '')
-      
-      department = itemList[4]
-      
-      department = department.replace(' style="width: 130px;">', '')
-      department = department.replace('>', '')
+      name = item['name']
+      department = item['description']
       print('发现新订单:' + itemID)
-      sendMessage("项目编号: %s\n项目名称: %s\n%s" % (itemID, name, ddxx(itemID)))
+      sendMessage("项目编号: %s\n项目名称: %s\n%s" % (itemID, name, ddxx(item)))
       with open('data.json', 'w') as f:
         f.write(json.dumps(orderList))
         f.close()
@@ -109,7 +73,9 @@ def weiqiang(text):
 def sendMessage(content):
   weixin.getToken()
   weixin.sendMessage({
+    # PuGe
     "touser" : "@all",
+    # "touser" : "PuGe",
     "msgtype" : "text",
     "agentid" : 1000002,
     "text" : {
@@ -124,11 +90,17 @@ def getPageCode():
   cookie_jar.set("PHPSESSID", cookiesData["PHPSESSID"], domain="project.peopleurl.cn")
   cookie_jar.set("sso_s", cookiesData["sso_s"], domain="peopleurl.cn")
   cookie_jar.set("sso_u", cookiesData["sso_u"], domain="peopleurl.cn")
-  response = requests.get("https://project.peopleurl.cn/partyb/main.php", cookies = cookie_jar)
+  response = requests.post("https://project.peopleurl.cn/interface/public/index.php/order/xqlists", '{"demand_type":1,"pageInfo":{"page":1,"pageSize":15,"total":1},"searchInfo":{"status":"","dep_id":"","name":"","form_type":""}}', cookies = cookie_jar)
   # print(response.text)
-  htmlData = clear(response.text)
-  dataList = Tool.subStringArr(htmlData, '<td>操作</td>', '</table>')
-  content = weiqiang(dataList[0])
+  resData = response.json()
+  # print(resData['data']['lists'])
+  content = weiqiang(resData['data']['lists'])
+  # 获取临时单子
+  response = requests.post("https://project.peopleurl.cn/interface/public/index.php/order/xqlists", '{"demand_type":2,"pageInfo":{"page":1,"pageSize":15,"total":1},"searchInfo":{"status":"","dep_id":"","name":"","form_type":""}}', cookies = cookie_jar)
+  # print(response.text)
+  resData = response.json()
+  # print(resData['data']['lists'])
+  content = weiqiang(resData['data']['lists'])
   alive.updata()
 
 
@@ -147,24 +119,7 @@ def getPageDeadLine(key):
     return returnText + key + '. ' + name + '\n'
   return ''
 
-# # 提醒编辑确认
-# def alertDeadLine():
-#   cookie_jar = RequestsCookieJar()
-#   cookie_jar.set("PHPSESSID", cookiesData["PHPSESSID"], domain="project.peopleurl.cn")
-#   response = requests.get("https://project.peopleurl.cn/partyb/list.php", cookies = cookie_jar)
-#   # print(response.text)
-#   # htmlData = clear(response.text)
-#   dataList = Tool.subStringArr(response.text, 'href="view.php?id=', '">')
-  
-#   # content = weiqiang(dataList[0])
-#   alertText = ''
-#   for key in dataList:
-#     # 判断是否过期
-#     alertText += getPageDeadLine(key)
-#   if (alertText != ''):
-#     sendMessage('有编辑未点击完成项目:\n' + alertText)
-
-# alertDeadLine()
+getPageCode()
 
 scheduler = BlockingScheduler(timezone="Asia/Shanghai")
 # 每60分钟获取cook
